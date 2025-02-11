97e1e9df76c77a9ccc3d5d457b1a5e372d23a6a8
mandscode <mandscode@gmail.com>
2025-02-11 17:45:30 +0530

    .env added

--- frontend/src/api/api.tsx
+++ frontend/src/api/api.tsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 // Define the base URL of your API and other configurations
 const api = axios.create({
   // baseURL: 'https://0198-2409-40e1-10f1-2d63-11ac-21be-2b33-ff11.ngrok-free.app/', // Replace with your actual API base URL
-  baseURL: 'http://13.233.146.212:3000', // Replace with your actual API base URL
+  baseURL: 'http://ec2-13-233-146-212.ap-south-1.compute.amazonaws.com:3000', // Replace with your actual API base URL
   // baseURL: 'http://localhost:3000', // Replace with your actual API base URL
   headers: {
     'Content-Type': 'application/json',
